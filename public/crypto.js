1de6d2e4a8a205e43b09202955f2df56b688c719
testcode369 <purnanandwasave.docs@gmail.com>
2025-05-31 05:17:58 +0530

    Create crypto.js

---
+++ public/crypto.js
@@ -0,0 +1,9 @@
+// crypto.js
+export function generateToken(length = 16) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let token = '';
+  for (let i = 0; i < length; i++) {
+    token += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return token;
+}
